18ed8785b5ac4d9acc26c72ff44f9de2fd790c2b
ehsanguru <ehsan.guru@icloud.com>
2015-09-15 16:52:16 +0600

    Update ubuntu_Initial.sh
    best commit

--- ubuntu_Initial.sh
+++ ubuntu_Initial.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
+#!/bin/sh
+#
 ###########
-apt-get update && apt-get upgrade && apt-get dist-upgrade -y
-apt-get autoremove && apt-get autoclean -y
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+apt-get autoclean -y
 apt-get install build-essential wget curl nano sudo -y
-sudo apt-get install linux-image-`uname -r`
+sudo apt-get install linux-image-`uname -r` -y
 reboot 
